5679fbc09f548f630053b673dfa32aeedea03452
Decent-Cypher <binhngdc1911@gmail.com>
2025-03-19 16:09:30 +0000

    Add a small test API to demonstrate connection from backend to frontend

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+  const [feedback, setFeedback] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/feedback/')
+      .then(response => {
+        setFeedback(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the feedback!', error);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>Home</h1>
-      <p>This is the about page.</p>
+      <h1>hello bro</h1>
+      <ul>
+        {feedback.map(item => (
+          <li key={item.id}>{item.comment} - {item.rating}</li>
+        ))}
+      </ul>
     </div>
   );
 };
