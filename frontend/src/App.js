35ecd3b93aaac21f41c1efb3ead03489f43eaab5
ankhanhtran02 <ankhanhtran02@gmail.com>
2025-03-26 00:19:59 +0700

    Added navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,21 @@ import {Routes, Route} from 'react-router-dom';
 import Home from './components/Home';
 import Menu from './components/Menu';
 import Order from './components/Order';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/order" element={<Order />} />
-      </Routes>
-    </div>
+    <>
+      <Navbar />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/order" element={<Order />} />
+        </Routes>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
