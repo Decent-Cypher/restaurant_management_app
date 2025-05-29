ec63c44e292e21d0bc26f0de5a73b23bf555d73c
Decent-Cypher <binhngdc1911@gmail.com>
2025-05-29 17:10:30 +0000

    Added testing for frontend pages

--- frontend/src/pages/routing.tsx
+++ frontend/src/pages/routing.tsx
@@ -1,24 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App'
-import Menu from './Menu'
-import Order from './Order'
-import NotFound from './404'
-import Login from './Login'
-import OrderSubpageLayout from '../components/OrderSubpageLayout'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Menu from './Menu';
+import Order from './Order';
+import NotFound from './404';
+import Login from './Login';
+import OrderSubpageLayout from '../components/OrderSubpageLayout';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/order" element={<Order />} />
-        <Route path="/order/drinks" element={<OrderSubpageLayout />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/order/drinks" element={<OrderSubpageLayout />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
+  );
+} else {
+  console.error("Root element not found");
+}
